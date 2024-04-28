cacdca995045b733b5eb2fb52d06806666839167
Rajendra Pandey <rajendrapandey.95@live.com>
2024-04-28 19:12:49 +0530

    Create Finding Pairs.cpp

---
+++ 2024/April/Finding Pairs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int n;
+        cin >> n;
+        array<int, 1000001> pos{};
+        array<int, 1000001> neg{};
+        for (long long int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            if (x >= 0)
+                pos[x]++;
+            else
+                neg[-x]++;
+        }
+        long long int ans = 0;
+        for (int i = 0; i <= 1000000; i++)
+        {
+            if (pos[i] > 0)
+            {
+                ans += (pos[i] * (pos[i] + 1) / 2);
+            }
+            if (neg[i] > 0)
+            {
+                ans += (neg[i] * (neg[i] + 1) / 2);
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
